e08f24a17cad3b0c3911b039610d23f6665726fb
hulrap <hulanraphael@gmail.com>
2025-06-17 22:33:48 +0200

    v9.6

--- components/DesktopLayout.tsx
+++ components/DesktopLayout.tsx
@@ -240,7 +240,7 @@ export const DesktopLayout: React.FC = () => {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* Desktop Icons */}
-      <div className="absolute top-0 left-12 p-4 space-y-6">
+      <div className="absolute top-0 left-12 p-4 pb-20 space-y-6 h-full overflow-y-auto">
         {WINDOW_ORDER.map((id) => (
           <DesktopIcon
             key={id}
